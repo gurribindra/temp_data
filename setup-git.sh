c9a0ab38f80f86aa99405c3d33403411dd04690e
Gurvinder Singh Bindra <gurribindra@gmail.com>
2025-05-14 14:31:22 +0000

    fixing stuff 1

--- setup-git.sh
+++ setup-git.sh
@@ -2,4 +2,4 @@ git config --global user.email "gurribindra@gmail.com"
 git config --global user.name "Gurvinder Singh Bindra"
 git config --global credential.helper store
 export git_user=gurribindra
-export git_password=$(cat git-token.secret)
+export git_password=$(cat temp_data.txt)
