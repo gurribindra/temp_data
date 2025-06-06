477a5cd816ad665131ae0ffc354ec33f8ec072ea
Gurvinder Singh Bindra <gurribindra@gmail.com>
2025-06-06 01:02:18 +0000

    first push

---
+++ setup_git.sh
@@ -0,0 +1,5 @@
+git config --global user.email "gurribindra@gmail.com"
+git config --global user.name "Gurvinder Singh Bindra"
+git config --global credential.helper store
+export git_user=gurribindra
+export git_password="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"
